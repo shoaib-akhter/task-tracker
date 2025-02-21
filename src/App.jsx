b7e62565247a0ec6934099b03b4f0380f75245d3
Shoaib Akhtar <56090706+shoaib-akhter@users.noreply.github.com>
2025-02-21 13:39:38 +0500

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
 
 function App() {
 
   return (
       <div>
-        <h1>Task Tracker</h1>
+        <Header />
+        {/* More content will go here soon */}
       </div>
   )
 }
